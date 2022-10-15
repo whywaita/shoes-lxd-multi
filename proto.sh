704438153f97387fb4da50b8b6a763896c58399a
whywaita <whywaita@whywrite.it>
2022-10-15 18:45:01 +0900

    Use proto.go in whywaita/myshoes

--- proto.sh
+++ proto.sh
@@ -2,6 +2,6 @@
 
 rm -rf ./tmp
 mkdir -p ./tmp/proto.go
-protoc -I . --go_out=tmp/proto.go --go-grpc_out=tmp/proto.go proto/*.proto
+protoc -I=proto --go_out=tmp/proto.go --go-grpc_out=tmp/proto.go proto/**/*.proto
 mv tmp/proto.go/github.com/whywaita/shoes-lxd-multi/proto.go/shoeslxdmulti/* ./proto.go
 rm -rf ./tmp
