aabd729c10a56fc4304600e655c1edc279b2a79d
site0801 <0801site@gmail.com>
2023-09-08 14:44:51 +0900

    Revert "Fix schedule()"
    This reverts commit 2e9bb96681564712291d635092117118bce10cac.

--- server/pkg/api/server_add_instance.go
+++ server/pkg/api/server_add_instance.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/rand"
 	"net/url"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
@@ -207,9 +208,15 @@ func schedule(targets []targetHost, limitOverCommit uint64) (*targetHost, error)
 		return nil, ErrNoValidHost
 	}
 
-	minTargets := getMinTargets(schedulableTargets)
+	// 1. use lowest over-commit instance
+	// 2. check limit of over-commit
+	sort.SliceStable(schedulableTargets, func(i, j int) bool {
+		// lowest percentOverCommit is first
+		return schedulableTargets[i].percentOverCommit < schedulableTargets[j].percentOverCommit
+	})
 
-	return &minTargets[rand.Intn(len(minTargets))], nil
+	index := rand.Intn(len(schedulableTargets))
+	return &schedulableTargets[index], nil
 }
 
 // parseAlias parse user input
@@ -247,20 +254,4 @@ func parseAlias(input string) (*api.InstanceSource, error) {
 		Type:  "image",
 		Alias: input,
 	}, nil
-}
-
-func getMinTargets(hosts []targetHost) []targetHost {
-	var minTargets []targetHost
-	// use lowest over-commit instance
-	// if there is more than one the lowest ones, it picks up from them randomly
-	minTargetOverCommit := hosts[0].percentOverCommit
-	for _, t := range hosts {
-		if minTargetOverCommit > t.percentOverCommit {
-			minTargetOverCommit = t.percentOverCommit
-			minTargets = []targetHost{t}
-		} else if minTargetOverCommit == t.percentOverCommit {
-			minTargets = append(minTargets, t)
-		}
-	}
-	return minTargets
 }
