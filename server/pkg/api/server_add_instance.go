98949bf3a448b280ab5001491fa2e5cfeb1a2e71
whywaita <whywaita@whywrite.it>
2024-03-06 17:11:17 +0900

    op is nil panic if error DeleteInstance

--- server/pkg/api/server_add_instance.go
+++ server/pkg/api/server_add_instance.go
@@ -84,6 +84,7 @@ func (s *ShoesLXDMultiServer) AddInstance(ctx context.Context, req *pb.AddInstan
 		op, err := client.DeleteInstance(instanceName)
 		if err != nil {
 			l.Warn("failed to delete instance", "err", err.Error())
+			return nil, status.Errorf(codes.Internal, "failed to wait starting instance: %+v", err)
 		}
 		if err := op.Wait(); err != nil {
 			l.Warn("failed to wait deleting instance", "err", err.Error())
@@ -97,6 +98,7 @@ func (s *ShoesLXDMultiServer) AddInstance(ctx context.Context, req *pb.AddInstan
 		op, err := client.DeleteInstance(instanceName)
 		if err != nil {
 			l.Warn("failed to delete instance", "err", err.Error())
+			return nil, status.Errorf(codes.Internal, "failed to wait starting instance: %+v", err)
 		}
 		if err := op.Wait(); err != nil {
 			l.Warn("failed to wait deleting instance", "err", err.Error())
