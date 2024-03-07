02458d1b5c5e13a34be581d4dbfd0f9fe1bae335
gamoutatsumi <tatsumi@gamou.dev>
2024-03-07 18:29:50 +0900

    add hostname to metrics

--- server/pkg/metric/log.go
+++ server/pkg/metric/log.go
@@ -7,6 +7,6 @@ import (
 var (
 	FailedAllocateCount = prometheus.NewCounterVec(
 		prometheus.CounterOpts{Namespace: namespace, Subsystem: lxdName, Name: "failed_allocate_count", Help: "failed allocate instance count"},
-		[]string{"lxd_host", "resource_type"},
+		[]string{"lxd_host", "resource_type", "host_name"},
 	)
 )
