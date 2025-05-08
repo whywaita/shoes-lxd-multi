86e4c9b0f791cd2bde667bf0280f266760024432
gamoutatsumi <tatsumi@gamou.dev>
2025-05-08 17:33:44 +0900

    write tests for adjust instances method

--- pool-agent/cmd/agent_test.go
+++ pool-agent/cmd/agent_test.go
@@ -2,10 +2,220 @@ package cmd_test
 
 import (
 	"testing"
+	"time"
 
+	lxd "github.com/lxc/lxd/client"
 	"github.com/lxc/lxd/shared/api"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 	"github.com/whywaita/shoes-lxd-multi/pool-agent/cmd"
 )
+
+type AgentSuite struct {
+	suite.Suite
+	agent *cmd.Agent
+}
+
+type mockLxdClient struct {
+	mock.Mock
+	lxd.InstanceServer
+}
+
+func (m *mockLxdClient) GetInstances(instanceType api.InstanceType) ([]api.Instance, error) {
+	args := m.Called(instanceType)
+	return args.Get(0).([]api.Instance), args.Error(1)
+}
+
+func (s *AgentSuite) SetupSuite() {
+	s.agent = &cmd.Agent{
+		Client: &mockLxdClient{},
+		Image: map[string]*cmd.Image{
+			"focal": {
+				InstanceSource: api.InstanceSource{},
+				Config: cmd.ConfigPerImage{
+					ImageAlias:          "ubuntu:focal",
+					ResourceTypesCounts: map[string]int{"typeA": 2, "typeB": 1, "typeC": 0},
+				},
+				Status: cmd.ImageStatus{
+					CreatingInstances: map[string]cmd.Instances{},
+					CurrentImage: struct {
+						Hash      string
+						CreatedAt time.Time
+					}{},
+				},
+			},
+			"noble": {
+				InstanceSource: api.InstanceSource{},
+				Config: cmd.ConfigPerImage{
+					ImageAlias:          "ubuntu:noble",
+					ResourceTypesCounts: map[string]int{"typeA": 2, "typeB": 0, "typeC": 1},
+				},
+				Status: cmd.ImageStatus{
+					CreatingInstances: map[string]cmd.Instances{},
+					CurrentImage: struct {
+						Hash      string
+						CreatedAt time.Time
+					}{},
+				},
+			},
+		},
+	}
+	s.agent.Client.(*mockLxdClient).On("GetInstances", api.InstanceTypeContainer).Return([]api.Instance{
+		{
+			Name:       "available_stock_running",
+			StatusCode: api.Running,
+			InstancePut: api.InstancePut{
+				Config: map[string]string{
+					cmd.ConfigKeyResourceType: "typeA",
+					cmd.ConfigKeyImageAlias:   "ubuntu:focal",
+					cmd.ConfigKeyRunnerName:   "runner1",
+				},
+			},
+		},
+		{
+			Name:       "available_stock_frozen",
+			StatusCode: api.Frozen,
+			InstancePut: api.InstancePut{
+				Config: map[string]string{
+					cmd.ConfigKeyResourceType: "typeB",
+					cmd.ConfigKeyImageAlias:   "ubuntu:focal",
+					cmd.ConfigKeyRunnerName:   "",
+				},
+			},
+		},
+		{
+			Name:       "broken_running",
+			StatusCode: api.Running,
+			InstancePut: api.InstancePut{
+				Config: map[string]string{
+					cmd.ConfigKeyResourceType: "typeC",
+					cmd.ConfigKeyImageAlias:   "ubuntu:focal",
+					cmd.ConfigKeyRunnerName:   "", // correctly running container should contain runner name
+				},
+			},
+		},
+		{
+			Name:       "disabled_stock_frozen",
+			StatusCode: api.Frozen,
+			InstancePut: api.InstancePut{
+				Config: map[string]string{
+					cmd.ConfigKeyResourceType: "typeD",
+					cmd.ConfigKeyImageAlias:   "ubuntu:focal",
+					cmd.ConfigKeyRunnerName:   "",
+				},
+			},
+		},
+	}, nil)
+}
+
+func (s *AgentSuite) TestCalculateToDeleteInstances() {
+	tests := []struct {
+		name             string
+		resourceTypeName string
+		version          string
+		want             bool
+	}{
+		{
+			name:             "typeA - focal",
+			resourceTypeName: "typeA",
+			version:          "focal",
+			want:             false,
+		},
+		{
+			name:             "typeB - focal",
+			resourceTypeName: "typeB",
+			version:          "focal",
+			want:             false,
+		},
+		{
+			name:             "typeC - focal",
+			resourceTypeName: "typeC",
+			version:          "focal",
+			want:             true,
+		},
+		{
+			name:             "typeD - focal - not configured",
+			resourceTypeName: "typeD",
+			version:          "focal",
+			want:             true,
+		},
+	}
+	instances, err := s.agent.Client.GetInstances(api.InstanceTypeContainer)
+	s.Require().NoError(err)
+	for _, tt := range tests {
+		disabledResourceTypes := []string{}
+		_, ok := s.agent.CalculateCreateCount(instances, tt.resourceTypeName, tt.version)
+		if !ok {
+			disabledResourceTypes = append(disabledResourceTypes, tt.resourceTypeName)
+		}
+		s.Run(tt.name, func() {
+			toDelete := s.agent.CalculateToDeleteInstances(instances, disabledResourceTypes, tt.version)
+			s.Equal(tt.want, len(toDelete) > 0)
+		})
+	}
+}
+
+func (s *AgentSuite) TestCalculateCreateCount() {
+	tests := []struct {
+		name             string
+		resourceTypeName string
+		version          string
+		want1            int
+		want2            bool
+	}{
+		{
+			name:             "typeA - focal not enough",
+			resourceTypeName: "typeA",
+			version:          "focal",
+			want1:            1,
+			want2:            true,
+		},
+		{
+			name:             "typeB - focal already created",
+			resourceTypeName: "typeB",
+			version:          "focal",
+			want1:            0,
+			want2:            true,
+		},
+		{
+			name:             "typeC - focal disabled",
+			resourceTypeName: "typeC",
+			version:          "focal",
+			want1:            0,
+			want2:            false,
+		},
+		{
+			name:             "typeA - noble not enough",
+			resourceTypeName: "typeA",
+			version:          "noble",
+			want1:            2,
+			want2:            true,
+		},
+		{
+			name:             "typeB - noble disabled",
+			resourceTypeName: "typeB",
+			version:          "noble",
+			want1:            0,
+			want2:            false,
+		},
+		{
+			name:             "typeC - noble not enough",
+			resourceTypeName: "typeC",
+			version:          "noble",
+			want1:            1,
+			want2:            true,
+		},
+	}
+	for _, tt := range tests {
+		s.Run(tt.name, func() {
+			instances, err := s.agent.Client.GetInstances(api.InstanceTypeContainer)
+			s.Require().NoError(err)
+			count, ok := s.agent.CalculateCreateCount(instances, tt.resourceTypeName, tt.version)
+			s.Equal(tt.want1, count)
+			s.Equal(tt.want2, ok)
+		})
+	}
+}
 
 func TestIsPooledInstance(t *testing.T) {
 	t.Parallel()
@@ -23,13 +233,13 @@ func TestIsPooledInstance(t *testing.T) {
 				InstancePut: api.InstancePut{
 					Config: map[string]string{
 						cmd.ConfigKeyResourceType: "typeA",
-						cmd.ConfigKeyImageAlias:   "image1",
+						cmd.ConfigKeyImageAlias:   "ubuntu:focal",
 						cmd.ConfigKeyRunnerName:   "",
 					},
 				},
 			},
 			resourceTypeName: "typeA",
-			imageAlias:       "image1",
+			imageAlias:       "ubuntu:focal",
 			want:             false,
 		},
 		{
@@ -39,13 +249,13 @@ func TestIsPooledInstance(t *testing.T) {
 				InstancePut: api.InstancePut{
 					Config: map[string]string{
 						cmd.ConfigKeyResourceType: "typeB",
-						cmd.ConfigKeyImageAlias:   "image1",
+						cmd.ConfigKeyImageAlias:   "ubuntu:focal",
 						cmd.ConfigKeyRunnerName:   "runner1",
 					},
 				},
 			},
 			resourceTypeName: "typeA",
-			imageAlias:       "image1",
+			imageAlias:       "ubuntu:focal",
 			want:             false,
 		},
 		{
@@ -55,13 +265,13 @@ func TestIsPooledInstance(t *testing.T) {
 				InstancePut: api.InstancePut{
 					Config: map[string]string{
 						cmd.ConfigKeyResourceType: "typeA",
-						cmd.ConfigKeyImageAlias:   "image2",
+						cmd.ConfigKeyImageAlias:   "ubuntu:noble",
 						cmd.ConfigKeyRunnerName:   "",
 					},
 				},
 			},
 			resourceTypeName: "typeA",
-			imageAlias:       "image1",
+			imageAlias:       "ubuntu:focal",
 			want:             false,
 		},
 		{
@@ -71,13 +281,13 @@ func TestIsPooledInstance(t *testing.T) {
 				InstancePut: api.InstancePut{
 					Config: map[string]string{
 						cmd.ConfigKeyResourceType: "typeA",
-						cmd.ConfigKeyImageAlias:   "image1",
+						cmd.ConfigKeyImageAlias:   "ubuntu:focal",
 						cmd.ConfigKeyRunnerName:   "runner1",
 					},
 				},
 			},
 			resourceTypeName: "typeA",
-			imageAlias:       "image1",
+			imageAlias:       "ubuntu:focal",
 			want:             true,
 		},
 		{
@@ -87,13 +297,13 @@ func TestIsPooledInstance(t *testing.T) {
 				InstancePut: api.InstancePut{
 					Config: map[string]string{
 						cmd.ConfigKeyResourceType: "typeA",
-						cmd.ConfigKeyImageAlias:   "image1",
+						cmd.ConfigKeyImageAlias:   "ubuntu:focal",
 						cmd.ConfigKeyRunnerName:   "",
 					},
 				},
 			},
 			resourceTypeName: "typeA",
-			imageAlias:       "image1",
+			imageAlias:       "ubuntu:focal",
 			want:             true,
 		},
 	}
@@ -106,4 +316,9 @@ func TestIsPooledInstance(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestAgentSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(AgentSuite))
 }
--- pool-agent/cmd/export_test.go
+++ pool-agent/cmd/export_test.go
@@ -4,3 +4,6 @@ var IsPooledInstance = isPooledInstance
 var ConfigKeyResourceType = configKeyResourceType
 var ConfigKeyImageAlias = configKeyImageAlias
 var ConfigKeyRunnerName = configKeyRunnerName
+
+type Instances = instances
+type ImageStatus = imageStatus
